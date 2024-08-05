7cdbeae7bf33f81eb39599877a932abd6492c7b9
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-08-05 20:54:54 +0530

    adding chat messages input widgets

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -21,7 +23,14 @@ class ChatScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(child: Text('Logged In!')),
+      body: const Center(
+        child: Column(
+          children: [
+            Expanded(child: ChatMessages()),
+            NewMessage(),
+          ],
+        ),
+      ),
     );
   }
 }
